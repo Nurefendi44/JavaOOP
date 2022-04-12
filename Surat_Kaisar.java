6eeb5d29fd37d184c0c838644e98079ffd1cc8eb
Nurefendi44 <103479574+Nurefendi44@users.noreply.github.com>
2022-04-12 12:43:26 +0700

    Add files via upload

---
+++ Surat_Kaisar.java
@@ -0,0 +1,42 @@
+package com.tugas1.Surat_Kaisar;
+import java.util.Scanner;
+class Main {
+    public static void methodcaesar(String klmtAsli, int n){
+        String ciphertext = "";
+        char alphabet;
+        for(int i=0; i < klmtAsli.length();i++) {
+            alphabet = klmtAsli.charAt(i);
+
+            if(alphabet >= 'a' && alphabet <= 'z') {
+                alphabet = (char) (alphabet + n);
+
+                if(alphabet > 'z') {
+                    alphabet = (char) (alphabet+'a'-'z'-1);
+                }
+                ciphertext = ciphertext + alphabet;
+
+            } else if(alphabet >= 'A' && alphabet <= 'Z') {
+                alphabet = (char) (alphabet + n);
+
+                if(alphabet > 'Z') {
+
+                    alphabet = (char) (alphabet+'A'-'Z'-1);
+                }
+                ciphertext = ciphertext + alphabet;
+            }
+
+            else {
+                ciphertext = ciphertext + alphabet;
+            }
+
+        }
+        System.out.println(ciphertext);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        String klmtAsli = sc.nextLine();
+        int n = sc.nextInt();
+        Main.methodcaesar(klmtAsli,n);
+    }
+}
